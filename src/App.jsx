4f24c85818f6122b50e690bcbeb198f58228050a
manojgarnayak <manojgarnayak07@gmail.com>
2024-01-20 09:56:45 +0530

    Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import Card from './components/cards'
 import './App.css'
 
 function App() {
-  let  myObj = {
-    username: "Manoj",
-    age: 23
-  }
-  let newArr = [1,2,3,4,5]
-
+  
   return (
     <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      <h1 className='bg-blue-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
       <Card username="Manoj" btnText = "click me"/>
       <Card username="Bhalu" />
     </>
